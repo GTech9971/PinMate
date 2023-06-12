cba0ed92c2562aefc24bb3837487f2f2e0409608
danbara01703 <georgetech1984@gmail.com>
2023-06-12 16:58:46 +0900

    ピンの足コンポーネントを作成

--- pic-mate-app/src/pages/Home.tsx
+++ pic-mate-app/src/pages/Home.tsx
@@ -1,7 +1,10 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Home.css';
+import { PinFootLeft } from '../components/PinFootLeft/PinFootLeft';
 
 const Home: React.FC = () => {
+
+
   return (
     <IonPage>
       <IonHeader>
@@ -10,7 +13,13 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
+        <svg style={{ width: '100vw', height: '50vh' }} >
+          <circle cx='25' cy='25' r='10' />
 
+          <PinFootLeft x={10} y={40} pinNo={0} />
+
+          <rect x='10' y='10' width='40vw' height='45vh' rx={10} fill='none' stroke='black' />
+        </svg>
       </IonContent>
     </IonPage>
   );
